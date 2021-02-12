078a8f15adb2ed0bef0a8a75d38aa6b3935a8309
Bisrat Belay <bisratb19@outlook.com>
2021-02-12 22:19:39 +0000

    lab06.sh v0.02

--- lab06.sh
+++ lab06.sh
@@ -15,3 +15,15 @@
 #Scp report log file back, rm script and log file
 #Continue to top of list or exit 0
 
+#----
+
+# 1
+
+IP_ADD_1=$1
+IDENTITY_FILE_1=~/id_file.pub
+
+for IP_ADD in $IP_ADD_1
+do
+	PORT_CHECKER=$(nc -zv $IP_ADD 22 | printf $?)
+	if [ $PORT_CHECKER < 1 ]; then
+		ssh -i $IDENTITY_FILE_1 ec2-user@$IP_ADD_1

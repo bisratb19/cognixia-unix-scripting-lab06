a4fa7bbe892aba7631c5c0899017667dca7a0960
Bisrat Belay <bisratb19@outlook.com>
2021-02-12 21:20:00 +0000

    lab06.sh v0.01

---
+++ lab06.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Argument of server IPs to check
+#Loop through one at a time
+
+# 1
+
+#Check if port 22 is open
+#SSH and print internal IP
+#-- Print failed report and continue to top of list
+
+# 2
+
+#Run system report on ssh instance
+#Scp report log file back, rm script and log file
+#Continue to top of list or exit 0
+

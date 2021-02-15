06f535e95aed3375a9ae3a21c6c9e08e9db5cfaa
Bisrat Belay <bisratb19@outlook.com>
2021-02-15 18:33:14 +0000

    lab06.sh v 3.01

--- lab06.sh
+++ lab06.sh
@@ -3,6 +3,7 @@
 #Lab 06 - February 15, 2021
 
 #Give IP address as argument
+#Provide ssh identify file path
 
 IP_ADD_1=$1
 IDENTITY_FILE_1=~/.ssh/chitra-feb21.pem
@@ -11,12 +12,19 @@ for IP_ADD in $IP_ADD_1
 do
 	PORT_CHECKER=$(nc -zv $IP_ADD 22 | printf $?)
 	if [ $PORT_CHECKER == 0 ];then
-		ssh -i $IDENTITY_FILE_1 ec2-user@$IP_ADD "printf $HOSTNAME > remote_hostname.txt;wget https://raw.githubusercontent.com/bisratb19/cognixia-unix-scripting-lab01/main/lab01.sh;chmod u+x lab01.sh;./lab01.sh;exit" #try it like this first
-		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/remote_hostname.txt ~/Desktop/test/remote_hostname.txt
-		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/testfile.txt ~/Desktop/test/testfile.txt #get the log file fix this
+		ssh -i $IDENTITY_FILE_1 ec2-user@$IP_ADD "hostname > remote_hostname.txt;wget https://raw.githubusercontent.com/bisratb19/cognixia-unix-scripting-lab01/main/lab01.sh;chmod u+x lab01.sh;./lab01.sh;ls -lart | grep report_*.log | cut -d' ' -f11 > logfile_name.txt;exit"
+		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/remote_hostname.txt ./remote_hostname.txt
+		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/logfile_name.txt ./logfile_name.txt
+		#cat logfile_name.txt into var or to command
+		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/$(
 	else
 		printf 'Port 22 is not open. Failed to connect and print a report'
 	fi
+	
+	ssh -i $IDENTITY_FILE_1 ec2-user@$IP_ADD "rm lab01.sh remote_hostname.txt logfile_name.txt;exit"
 done
-#delete files on server
 exit 0
+
+#delete the files on server
+#get the correct log file
+#print IP in report log file -- not working

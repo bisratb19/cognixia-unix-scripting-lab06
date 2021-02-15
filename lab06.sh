819a9c798c663f68d6cc2da8bf16d7bd98c541b7
Bisrat Belay <bisratb19@outlook.com>
2021-02-15 17:15:50 +0000

    lab06.sh v1.01

--- lab06.sh
+++ lab06.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
+#Author: Bisrat Belay
+#Lab 06 - February 15, 2021
 
-#Argument of server IPs to check
-#Loop through one at a time
-
-# 1
-
-#Check if port 22 is open
-#SSH and print internal IP
-#-- Print failed report and continue to top of list
-
-# 2
-
-#Run system report on ssh instance
-#Scp report log file back, rm script and log file
-#Continue to top of list or exit 0
-
-#----
-
-# 1
+#Give IP address as argument
 
 IP_ADD_1=$1
-IDENTITY_FILE_1=~/id_file.pub
+IDENTITY_FILE_1=~/.ssh/chitra-feb21.pem
 
 for IP_ADD in $IP_ADD_1
 do
 	PORT_CHECKER=$(nc -zv $IP_ADD 22 | printf $?)
-	if [ $PORT_CHECKER < 1 ]; then
-		ssh -i $IDENTITY_FILE_1 ec2-user@$IP_ADD "printf $HOSTNAME;wget https://raw.githubusercontent.com/bisratb19/cognixia-unix-scripting-lab01/main/lab01.sh;./lab01.sh;exit" #try it like this first
-		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/report_*.log ~/. #fix file name issues
+	if [ $PORT_CHECKER == 0 ];then
+		ssh -i $IDENTITY_FILE_1 ec2-user@$IP_ADD "printf $HOSTNAME > remote_hostname.txt;wget https://raw.githubusercontent.com/bisratb19/cognixia-unix-scripting-lab01/main/lab01.sh;chmod u+x lab01.sh;./lab01.sh;exit" #try it like this first
+		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/remote_hostname.txt ~/Desktop/test/remote_hostname.txt
+		scp -i $IDENTITY_FILE_1 ec2-user@$IP_ADD:~/testfile.txt ~/Desktop/test/testfile.txt #get the log file fix this
 	else
 		printf 'Port 22 is not open. Failed to connect and print a report'
 	fi
 done
+#delete files on server
 exit 0
